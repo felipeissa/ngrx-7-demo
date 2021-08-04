8541a23d21ef34e86c4efc3d7568906f868ef6aa
Felipe Issa <felipe.issa@ericsson.com>
2021-08-04 09:05:35 -0300

    Facades test implementation

--- src/app/current/store/current-facade.service.spec.ts
+++ src/app/current/store/current-facade.service.spec.ts
@@ -1,10 +1,14 @@
 import { TestBed } from '@angular/core/testing';
+import { Store } from '@ngrx/store';
 import { provideMockStore } from '@ngrx/store/testing';
 
 import { CurrentFacadeService } from './current-facade.service';
+import { decrement, increment, incrementBy, reset } from './current.actions';
+import { getCurrentCount } from './current.selectors';
 
 describe('CurrentFacadeService', () => {
   let service: CurrentFacadeService;
+  let store: Store;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -13,9 +17,65 @@ describe('CurrentFacadeService', () => {
       ]
     });
     service = TestBed.inject(CurrentFacadeService);
+    store = TestBed.inject(Store);
   });
+
+
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('dispatch', () => {
+
+    beforeEach(() => {
+      spyOn(store, 'dispatch');
+    })
+
+    it('should dispatch increment action', () => {
+      const action = increment();
+
+      service.increment();
+
+      expect(store.dispatch).toHaveBeenCalledWith(action);
+    })
+
+    it('should dispatch decrement action', () => {
+      const action = decrement();
+
+      service.decrement();
+
+      expect(store.dispatch).toHaveBeenCalledWith(action);
+    })
+
+    it('should dispatch reset action', () => {
+      const action = reset();
+
+      service.reset();
+
+      expect(store.dispatch).toHaveBeenCalledWith(action);
+    })
+
+    it('should dispatch incrementBy action', () => {
+      const increment = 1;
+      const action = incrementBy({ increment });
+
+      service.incrementBy(increment);
+
+      expect(store.dispatch).toHaveBeenCalledWith(action);
+    })
+  })
+
+  describe('selectors', () => {
+    let spy;
+    beforeEach(() => {
+      spy = spyOn(store, 'select');
+    });
+
+    it('should select getCurrentCount', () => {
+      service.getCounter();
+
+      expect(store.select as any).toHaveBeenCalledWith(getCurrentCount);
+    });
   });
 });
--- src/app/legacy/store/legacy-facade.service.spec.ts
+++ src/app/legacy/store/legacy-facade.service.spec.ts
@@ -1,10 +1,15 @@
+import { asLiteral } from '@angular/compiler/src/render3/view/util';
 import { TestBed } from '@angular/core/testing';
+import { Store } from '@ngrx/store';
 import { provideMockStore } from '@ngrx/store/testing';
 
 import { LegacyFacadeService } from './legacy-facade.service';
+import { Decrement, Increment, IncrementBy, Reset } from './legacy.actions';
+import { getCounter } from './legacy.selectors';
 
 describe('LegacyFacadeService', () => {
   let service: LegacyFacadeService;
+  let store: Store;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -13,9 +18,63 @@ describe('LegacyFacadeService', () => {
       ]
     });
     service = TestBed.inject(LegacyFacadeService);
+    store = TestBed.inject(Store);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('dispatch', () => {
+
+    beforeEach(() => {
+      spyOn(store, 'dispatch');
+    })
+
+    it('should dispatch Increment action', () => {
+      const action = new Increment();
+
+      service.increment();
+
+      expect(store.dispatch).toHaveBeenCalledWith(action);
+    })
+
+    it('should dispatch Decrement action', () => {
+      const action = new Decrement();
+
+      service.decrement();
+
+      expect(store.dispatch).toHaveBeenCalledWith(action);
+    })
+
+    it('should dispatch Reset action', () => {
+      const action = new Reset();
+
+      service.reset();
+
+      expect(store.dispatch).toHaveBeenCalledWith(action);
+    })
+
+    it('should dispatch IncrementBy action', () => {
+      const increment = 1;
+      const action = new IncrementBy({ increment });
+
+      service.incrementBy(increment);
+
+      expect(store.dispatch).toHaveBeenCalledWith(action);
+    })
+  })
+
+  describe('selectors', () => {
+    let spy;
+    beforeEach(() => {
+      spy = spyOn(store, 'select');
+    });
+
+    it('should select getCounter', () => {
+      service.getCounter();
+
+      expect(store.select as any).toHaveBeenCalledWith(getCounter);
+    });
   });
 });
